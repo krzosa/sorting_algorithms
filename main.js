d7f5b8a4a1add75be6077a80dba99ec57780ccd4
krzosa <krzosa.karol@gmail.com>
2020-03-14 13:55:22 +0100

    working version, refactor

--- main.js
+++ main.js
@@ -1,64 +1,68 @@
+class SortRenderer{
+    constructor(){
+        this.canvas = document.getElementById('graph')
+        this.width = this.canvas.width = 800
+        this.height = this.canvas.height = 600
+        this.context = this.canvas.getContext('2d')
 
-const clearCanvas = () => {
-    context.fillStyle = 'rgb(100,100,100)'
-    context.fillRect(0,0,width,height)
-}
+        this.barSize = 5
+        this.animationSpeed = 5
+        this.barColor = '#333333'
+        this.bgColor = 'rgb(100,100,100)'
+        this.barWidth = 10
+        this.barGap = 0.5
+        this.toSortListSize = 50
+        this.toSortListMaxValue = 100
+        this.toSortlist = []
+    }
 
-const generateListRand = (listSize) => {
-    let numbers = []
-    for(let i = 0;i<listSize;i++)
-        numbers.push(Math.floor(Math.random()*100))
-    return numbers
-}
+    clearCanvas(){
+        this.context.fillStyle = this.bgColor
+        this.context.fillRect(0,0,this.width,this.height)
+    }
+    
+    generateListRand(){
+        let numbers = []
+        for(let i = 0;i<this.toSortListSize;i++)
+            numbers.push(Math.floor(Math.random()*this.toSortListMaxValue))
+        return numbers
+    }
 
-let bubbleSort = (nums) => {
-    for(let i = 0; i<nums.length-1; i++)
-        for(let j = 0;j<nums.length-1;j++){
-            if(nums[j+1] < nums[j]){
-                // swap elements
-                let temp = nums[j]
-                nums[j] = nums[j+1]
-                nums[j+1] = temp
+    bubbleSort(){
+        let nums = this.toSortlist
+        for(let i = 0; i<nums.length-1; i++)
+        setTimeout(() => {
+            for(let j = 0;j<nums.length-1;j++){
+                setTimeout(() => {
+                if(nums[j+1] < nums[j]){
+                    // swap elements
+                    let temp = nums[j]
+                    nums[j] = nums[j+1]
+                    nums[j+1] = temp
+                }
+                this.clearCanvas()
+                this.drawNumberList(nums, j)
+            }, this.animationSpeed*j)
             }
-            clearCanvas()
-            drawNumberList(nums)
-        }
-    return nums
-}
-
-const drawNumberBar = (num, pos) => {
-    pos = (pos*10)+200
-    context.fillStyle = '#333333';
-    context.fillRect(pos, 50, 10, 5*num)      
-}
-
-const drawNumberList = (nums) => {
-    for(let i = 0; i<nums.length; i++){
-        drawNumberBar(nums[i], i)
+        }, i*this.animationSpeed*nums.length)
+    }
+    
+    drawNumberBar(num, pos){
+        pos = (pos*(this.barWidth+this.barGap))
+        this.context.fillStyle = this.barColor;
+        this.context.fillRect(pos, this.barSize*100+20, this.barWidth, this.barSize*-num)          
+    }
+    
+    drawNumberList(nums, j){
+        for(let i = 0; i<nums.length; i++)
+        this.drawNumberBar(nums[i], i, this.barColor)
+    }
+    
+    renderSort(){
+        this.toSortlist = this.generateListRand(this.toSortListSize)
+        this.bubbleSort()
     }
 }
 
-
-const canvas = document.getElementById('graph');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight/1.5;
-const context = canvas.getContext('2d');
-
-context.fillStyle = 'rgb(100,100,100)'
-context.fillRect(0,0,width,height)
-
-
-numbers = generateListRand(50)
-
-numbers = bubbleSort(numbers)
-
-function start(counter){
-    if(counter < 10){
-      setTimeout(function(){
-        counter++;
-        console.log(counter);
-        start(counter);
-      }, 1000);
-    }
-  }
-  start(0);
+const a = new SortRenderer()
+a.renderSort()
